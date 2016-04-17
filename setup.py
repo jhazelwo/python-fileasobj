336b6bf2c0da6bea22636d294be046b83496320d
John Hazelwood <jhazelwo@users.noreply.github.com>
2016-04-17 11:18:46 -0600

    Sun Apr 17 11:18:46 MDT 2016

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name='fileasobj',
   packages=['fileasobj'],
-  version='5.0.0',
+  version='1.0.1',
   description='Manage a file as a Python list.',
   author='John Hazelwood',
   author_email='jhazelwo@users.noreply.github.com',
   url='https://github.com/jhazelwo/python-fileasobj',
-  download_url='https://github.com/jhazelwo/python-fileasobj/tarball/1.0.0',
+  download_url='https://github.com/jhazelwo/python-fileasobj/tarball/1.0.1',
   keywords=['python', 'file'],
   classifiers=[],
 )
